27a2eef43d210b2cbeba30b375c221ca29b39a6f
ruanjin <1731746380@qq.com>
2019-06-11 15:03:20 +0800

    init

---
+++ src/main/java/com/rj/Demo.java
@@ -0,0 +1,12 @@
+package com.rj;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by ruanj on 2019/6/11.
+ */
+public class Demo {
+
+    private final Logger logger = LoggerFactory.getLogger(Demo.class);
+}
---
+++ src/main/java/com/rj/config/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package com.rj.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * Created by ruanj on 2019/6/11.
+ */
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter(){
+        return new ServerEndpointExporter();
+    }
+}
